ed2b4fa546186cf223a6571ce323a445c1ba9188
Aditi <2306007@kiit.ac.in>
2026-07-03 02:29:52 +0530

    Day 1 - Spring Boot project setup with MySQL connection

---
+++ src/main/java/com/tatasteel/inventorybooking/InventoryBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tatasteel.inventorybooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryBookingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryBookingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tatasteel/inventorybooking/InventoryBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tatasteel.inventorybooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryBookingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
